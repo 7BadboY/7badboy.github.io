d818050f8e040b483950df3a6dda4d29bd4af32e
Yarotskiy Sergey <abrawa@gmail.com>
2019-01-26 22:45:20 +0200

    addToRepos

---
+++ app.js
@@ -0,0 +1,10 @@
+var productsCount = document.getElementById('products-count')
+var addToCartButtons = document.querySelectorAll('.add-to-cart')
+
+for (var i = 0; i < addToCartButtons.length; i++) {
+	addToCartButtons[i].addEventListener('click',function() {
+		var prevProductsCount = +productsCount.textContent
+		productsCount.textContent = prevProductsCount + 1
+		})
+}
+$('slider').slick()
